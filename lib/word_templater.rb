850392ff69c5897ea8d1430213d61cb339b93939
Mike Ries <michael@riesd.com>
2012-11-01 07:11:44 -0600

    refactoring to isolate the decisions into separate methods

--- lib/word_templater.rb
+++ lib/word_templater.rb
@@ -14,13 +14,6 @@ class WordTemplater
   # send_data buffer.string, :filename => 'REPC.docx'
   # # Or save the output to a word file
   # File.open("path/to/mydocument.docx", "wb") {|f| f.write(buffer.string) }
-
-  def self.available_tags
-    {
-      :example1 => '943 E Sunset Ave',
-      :example_address2 => 'Apt #4'
-    }
-  end
   
   def self.all_tags_regex
     /\|\|\<*.+?\>*\|\|/
@@ -38,60 +31,67 @@ class WordTemplater
     /(?<=>)(\w{3,})/
   end
   
+  def self.entry_requires_replacement?(entry)
+    entry.ftype != :directory && entry.name =~ /document|header|footer/
+  end
+  
+  def self.get_entry_content(entry, data_provider)
+    if self.entry_requires_replacement?(entry)
+      self.replace_entry_content(entry.get_input_stream.read, data_provider)
+    else
+      entry.get_input_stream.read
+    end
+  end
+  
+  def self.process_entry(entry, output, data_provider)
+    output.put_next_entry(entry.name)
+    output.write self.get_entry_content(entry, data_provider) if entry.ftype != :directory
+  end
+  
+  def self.replace_entry_content(str, data_provider)
+    possible_tags = str.scan(all_tags_regex)
+    # Loops through what looks like are tags. Anything with ||name|| even if they are not in the available tags list
+    possible_tags.each do |tag|
+      #extracts just the tag name
+      tag_name = self.malformed_tag_regex.match(tag)
+      tag_name ||= self.well_formed_tag_regex.match(tag)
+      tag_name ||= ''
+      # This will handle instances where someone edits just part of a tag and Word wraps that part in more XML
+      words = tag.scan(self.just_label_regex).flatten!
+      if words.respond_to?(:size) && words.size > 1
+        #Then the tag was split by word
+        tag_name = words.join('')
+      end
+      tag_name = tag_name.to_s.to_sym
+      # if in the available tag list, replace with the new value
+      if data_provider.has_key?(tag_name)
+        str.gsub!(tag, "#{data_provider[tag_name]}")
+      end
+    end
+    str
+  end
+  
   # Can pass in the same arguments here for available_tags as in the params for generate_tags_for
   def self.replace_file_with_content(file_path, *available_tags)
     # Rubyzip doesn't save it right unless saved like this: https://gist.github.com/e7d2855435654e1ebc52
     zf = Zip::ZipFile.new(file_path) # Put original file name here
     
-    available_tags ||= self.available_tags
     if available_tags.is_a?(Array)
       available_tags = self.generate_tags_for(available_tags)
     end
     buffer = Zip::ZipOutputStream.write_buffer do |out|
       zf.entries.each do |e|
-        if e.ftype == :directory
-          out.put_next_entry(e.name)
-        else
-          file_content = e.get_input_stream.read
-          out.put_next_entry(e.name)
-          # If this is the xml file with actual content
-          if e.name == 'word/document.xml' || e.name.include?('header') || e.name.include?('footer')
-            possible_tags = file_content.scan(all_tags_regex)
-            # Loops through what looks like are tags. Anything with ||name|| even if they are not in the available tags list
-            possible_tags.each do |tag|
-              #extracts just the tag name
-              tag_name = self.malformed_tag_regex.match(tag)
-              tag_name ||= self.well_formed_tag_regex.match(tag)
-              tag_name ||= ''
-              # This will handle instances where someone edits just part of a tag and Word wraps that part in more XML
-              words = tag.scan(self.just_label_regex).flatten!
-              if words.respond_to?(:size) && words.size > 1
-                #Then the tag was split by word
-                tag_name = words.join('')
-              end
-              tag_name = tag_name.to_s.to_sym
-              # if in the available tag list, replace with the new value
-              if available_tags.has_key?(tag_name)
-                file_content.gsub!(tag, "#{available_tags[tag_name]}")
-              end
-            end
-          end
-          out.write file_content
-        end
+        self.process_entry(e, out, available_tags)
       end
     end
     # You can save this buffer or send it with rails via send_data
     return buffer
   end
   
-  # Pass in records and this will return a list of available tags for all of the models
-  # Can also pass in a non mongoid record such as a hash and it will be automatically merged into the tags hash
-  # Can also include a Hash with :data => Mongoid record, :prefix => 'custom_prefix'
-  # Example WordTemplater.generate_tags_for(@deal.property, {:client_first_name => 'Paul'}, {:data => @deal.user, :prefix => 'client'}))
   def self.generate_tags_for(*args)
     attributes = {}
     args.flatten!
-    # Prefixes the model name or custom prefix. Makes it so we don't having naming clashes when used with records from multiple models
+    # Prefixes the model name or custom prefix. Makes it so we don't having naming clashes when used with records from multiple m
     args.each do |arg|
       if arg.is_a?(Hash) && arg.has_key?(:data) && arg.has_key?(:prefix)
         template_attributes = (arg[:data].respond_to?(:template_attributes) && :template_attributes) || :attributes
@@ -109,5 +109,4 @@ class WordTemplater
     end
     attributes
   end
-  
 end
