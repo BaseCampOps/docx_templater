5d618dbfd3fe864f0a958b46f33616be29e03a3f
Paul Smith <catalystmediastudios@gmail.com>
2013-02-01 16:45:03 -0700

    Stubs out the has_key? method in the mock objects.

--- spec/lib/placeholder_observer_spec.rb
+++ spec/lib/placeholder_observer_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 describe Docx::PlaceholderObserver do
   describe "#next_node" do
     subject{ Docx::PlaceholderObserver.new(data_provider) }
-    let(:data_provider){ r = double("data_provider") }
+    let(:data_provider) do 
+      r = double("data_provider")
+      r.stub(:has_key? => true)
+      r
+    end
     it "finds placeholders as it is given text nodes" do
       data_provider.should_receive(:[]).with(:title).and_return("The Thing")
       n1 = REXML::Text.new("dflkja sdf ||title|| slkjasdlkj")
